437a394c3dc5e26899a9d82e56c40a937f32323e
quentin cyrus <qcyrus3309@gmail.com>
2019-09-11 11:22:23 -0500

    Add new item functionality complete

--- ToDoIt/ToDoITViewController.swift
+++ ToDoIt/ToDoITViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  ToDoIt
 //
-//  Created by John M. Simone on 8/29/19.
+//  Created by Quebtin Cyrus on 8/29/19.
 //  Copyright © 2019 Quentin Cyrus. All rights reserved.
 //
 
@@ -10,22 +10,26 @@ import UIKit
 
 class ToDoItViewController: UITableViewController{
     
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon", "Sing Karaoke"]
+    // create new item array
+    
+    var itemArray = ["Design Tutorial App", "Put App on App Store", "Sell App", "Make Dat Money"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
     //Mark - Tableview Datasource Methods
+    
+    // number of rows in section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return itemArray.count // creates specific number of cells
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath) // reusable cell
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row] // set text label property
         
         return cell
     }
@@ -33,20 +37,59 @@ class ToDoItViewController: UITableViewController{
     //MARK - Tableview Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(itemArray[indexPath.row])
+//        print(itemArray[indexPath.row]) prints row to console
         
-//        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark prints corresponding item to console
         
-        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-             tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark { // create checkmark as an accessory
+             tableView.cellForRow(at: indexPath)?.accessoryType = .none // conditional to check and uncheck items
         }
         
         else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
-        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true) // gets rid of highlighting item in gray
     }
+    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+       
+        
+        var textField = UITextField() // create a text field
+        
+        let alert = UIAlertController(title: "Add New ToDoIt Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what will happen when user clicks the Add Item button on our UIAlert
+            // print(textField.text)  prints success to debug console
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+           
+        }
+        
+        alert.addTextField { (alertTextField) in  // create new text field to alert
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+            
+            print(alertTextField.text)
+            
+            
+        }
+        
+        
+        alert.addAction(action) // add action
+        
+        present(alert, animated: true, completion: nil) // show alert
+        
+    }
+    
+    
+    
 
 
 }
